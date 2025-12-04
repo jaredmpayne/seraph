774624d28e532b370a57bdcbe8367200c4eed610
Jared Payne <jaredmpayne@gmail.com>
2025-12-04 06:20:51 -0500

    Move detail to related class's file

--- seraph/event/AnyEvent.hpp
+++ seraph/event/AnyEvent.hpp
@@ -2,7 +2,31 @@
 
 #include <variant>
 
-#include <seraph/event/detail/AnyEventVariant.hpp>
+#include <seraph/event/FocusGainedEvent.hpp>
+#include <seraph/event/FocusLostEvent.hpp>
+#include <seraph/event/KeyPressedEvent.hpp>
+#include <seraph/event/KeyReleasedEvent.hpp>
+#include <seraph/event/MouseMovedEvent.hpp>
+#include <seraph/event/MousePressedEvent.hpp>
+#include <seraph/event/MouseReleasedEvent.hpp>
+#include <seraph/event/WindowClosedEvent.hpp>
+#include <seraph/event/WindowResizedEvent.hpp>
+
+namespace detail {
+
+using AnyEventVariant = std::variant<
+    FocusGainedEvent,
+    FocusLostEvent,
+    KeyPressedEvent,
+    KeyReleasedEvent,
+    MouseMovedEvent,
+    MousePressedEvent,
+    MouseReleasedEvent,
+    WindowClosedEvent,
+    WindowResizedEvent
+>;
+
+}
 
 class AnyEvent : public detail::AnyEventVariant {
 
