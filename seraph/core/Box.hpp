bafa856e9f8667a74fd1bbe71fe63b4f6e706c0b
Jared Payne <jaredmpayne@gmail.com>
2026-01-11 06:30:50 -0500

    Update Box documentation

--- seraph/core/Box.hpp
+++ seraph/core/Box.hpp
@@ -3,16 +3,20 @@
 #include <memory>
 
 /// A container for storing copyable objects on the heap.
+///
+/// This object is effectively a wrapper around an `std::unique_ptr` that
+/// creates a new copy of the underlying object with its own `std::unique_ptr`
+/// when copied.
 template <typename T>
 class Box {
 
 public:
 
-    constexpr Box(const Box<T> &other) noexcept :
-        Box(other.value()) { }
-
     constexpr Box(const T &value) noexcept :
         m_value(std::make_unique<T>(value)) { }
+
+    constexpr Box(const Box<T> &other) noexcept :
+        Box(other.value()) { }
 
     constexpr const T &value() const noexcept {
         return *m_value;
