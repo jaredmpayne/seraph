4fa22b68219d32af255b61dd1ce6b669ac24c17e
Jared Payne <jaredmpayne@gmail.com>
2026-01-11 06:30:19 -0500

    Add a few basic tests for Point

--- tests/core/PointTests.cpp
+++ tests/core/PointTests.cpp
@@ -1,3 +1,25 @@
 #include <tests/catch.hpp>
 
 #include <seraph/core/Point.hpp>
+
+TEST_CASE("point construction", "[Point]") {
+    
+    SECTION("default points are constructed with coordinates (0, 0)") {
+        const auto point = Point();
+        REQUIRE(point.x() == 0.0f);
+        REQUIRE(point.y() == 0.0f);
+    }
+
+    SECTION("points can be constructed memberwise") {
+        const auto point = Point(1.2f, 3.4f);
+        REQUIRE(point.x() == 1.2f);
+        REQUIRE(point.y() == 3.4f);
+    }
+
+    SECTION("points can be copied") {
+        const auto point = Point(1.2f, 3.4f);
+        const auto other = Point(point);
+        REQUIRE(other.x() == point.x());
+        REQUIRE(other.y() == point.y());
+    }
+}
