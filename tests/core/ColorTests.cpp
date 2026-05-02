a46ae8453a58b26f4797b539250224e75910e2e2
Jared Payne <jaredmpayne@gmail.com>
2026-05-02 05:19:17 -0400

    Reorganize tests

--- tests/core/ColorTests.cpp
+++ tests/core/ColorTests.cpp
@@ -2,13 +2,82 @@
 
 #include <seraph/core/Color.hpp>
 
-TEST_CASE("Color construction", "[Color]") {
+TEST_CASE("Color", "[Color]") {
     
-    SECTION("default Colors are the color black") {
-        REQUIRE(Color() == Color::black());
+    SECTION("Color()") {
+        const auto color = Color();
+        REQUIRE(color == Color(0, 0, 0, 255));
     }
 
-    SECTION("Colors can be constructed memberwise") {
+    SECTION("Color(r, g, b)") {
+        const auto color = Color(1, 2, 3);
+        REQUIRE(color == Color(1, 2, 3, 255));
+    }
+
+    SECTION("Color(r, g, b, a)") {
+        const auto color = Color(4, 5, 6, 7);
+        REQUIRE(color == Color(4, 5, 6, 7));
+    }
+
+    SECTION("Color(other)") {
+        const auto color = Color(1, 2, 3, 4);
+        const auto other = Color(color);
+        REQUIRE(color == other);
+    }
+
+    SECTION("operator<=>(other)") {
+        const auto small = Color(1, 2, 3, 4);
+        const auto large = Color(5, 6, 7, 8);
+        REQUIRE(small < large);
+        REQUIRE(large > small);
+        REQUIRE(small <= small);
+        REQUIRE(small <= large);
+        REQUIRE(large >= small);
+        REQUIRE(large >= large);
+        REQUIRE(small == small);
+        REQUIRE(small != large);
+        REQUIRE((small <=> large) == std::strong_ordering::less);
+        REQUIRE((small <=> small) == std::strong_ordering::equal);
+        REQUIRE((large <=> small) == std::strong_ordering::greater);
+    }
+
+    SECTION("black()") {
+        REQUIRE(Color::black() == Color(0, 0, 0, 255));
+    }
+
+    SECTION("red()") {
+        REQUIRE(Color::red() == Color(255, 0, 0, 255));
+    }
+
+    SECTION("green()") {
+        REQUIRE(Color::green() == Color(0, 255, 0, 255));
+    }
+
+    SECTION("blue()") {
+        REQUIRE(Color::blue() == Color(0, 0, 255, 255));
+    }
+
+    SECTION("yellow()") {
+        REQUIRE(Color::yellow() == Color(255, 255, 0, 255));
+    }
+
+    SECTION("cyan()") {
+        REQUIRE(Color::cyan() == Color(0, 255, 255, 255));
+    }
+
+    SECTION("magenta()") {
+        REQUIRE(Color::magenta() == Color(255, 0, 255, 255));
+    }
+
+    SECTION("white()") {
+        REQUIRE(Color::white() == Color(255, 255, 255, 255));
+    }
+
+    SECTION("transparent()") {
+        REQUIRE(Color::transparent() == Color(0, 0, 0, 0));
+    }
+
+    SECTION("getters") {
         const auto color = Color(1, 2, 3, 4);
         REQUIRE(color.r() == 1);
         REQUIRE(color.g() == 2);
@@ -16,115 +85,26 @@ TEST_CASE("Color construction", "[Color]") {
         REQUIRE(color.a() == 4);
     }
 
-    SECTION("Colors can be constructed with the alpha argument omitted") {
-        const auto color = Color(1, 2, 3);
-        REQUIRE(color.r() == 1);
-        REQUIRE(color.g() == 2);
-        REQUIRE(color.b() == 3);
-        REQUIRE(color.a() == 255);
+    SECTION("setters") {
+        auto color = Color();
+        color.set_r(1);
+        color.set_g(2);
+        color.set_b(3);
+        color.set_a(4);
+        REQUIRE(color == Color(1, 2, 3, 4));
     }
 
-    SECTION("Colors can be copied") {
-        const auto color = Color(1, 2, 3, 4);
-        const auto other = Color(color);
-        REQUIRE(other.r() == color.r());
-        REQUIRE(other.g() == color.g());
-        REQUIRE(other.b() == color.b());
-        REQUIRE(other.a() == color.a());
-    }
-}
-
-TEST_CASE("static Colors", "[Color]") {
-
-    SECTION("Color::black() returns the color black") {
-        REQUIRE(Color::black() == Color(0, 0, 0, 255));
-    }
-
-    SECTION("Color::red() returns the color red") {
-        REQUIRE(Color::red() == Color(255, 0, 0, 255));
-    }
-
-    SECTION("Color::green() returns the color green") {
-        REQUIRE(Color::green() == Color(0, 255, 0, 255));
-    }
-
-    SECTION("Color::blue() returns the color blue") {
-        REQUIRE(Color::blue() == Color(0, 0, 255, 255));
-    }
-
-    SECTION("Color::yellow() returns the color yellow") {
-        REQUIRE(Color::yellow() == Color(255, 255, 0, 255));
-    }
-
-    SECTION("Color::cyan() returns the color cyan") {
-        REQUIRE(Color::cyan() == Color(0, 255, 255, 255));
-    }
-
-    SECTION("Color::magenta() returns the color magenta") {
-        REQUIRE(Color::magenta() == Color(255, 0, 255, 255));
-    }
-
-    SECTION("Color::white() returns the color white") {
-        REQUIRE(Color::white() == Color(255, 255, 255, 255));
-    }
-
-    SECTION("Color::transparent() returns the color black with a zeroed alpha component") {
-        REQUIRE(Color::transparent() == Color(0, 0, 0, 0));
-    }
-}
-
-TEST_CASE("Color comparison", "[Color]") {
-
-    SECTION("Colors can be checked for equality") {
-        REQUIRE(Color() == Color(0, 0, 0, 255));
-        REQUIRE(Color::red() == Color(255, 0, 0, 255));
-    }
-
-    SECTION("Colors can be checked for non-equality") {
-        REQUIRE(Color() != Color::blue());
-        REQUIRE(Color::black() != Color::transparent());
-    }
-
-    SECTION("Colors can be compared memberwise in the order r, g, b, a") {
-        REQUIRE(Color(0, 255, 255, 255) < Color(255, 0, 0, 0));
-        REQUIRE(Color(0, 255, 0, 0) > Color(0, 128, 0, 0));
-    }
-}
-
-TEST_CASE("setting Color members after construction", "[Color]") {
-
-    auto color = Color();
-
-    SECTION("r can be set") {
-        color.set_r(128);
-        REQUIRE(color.r() == 128);
-    }
-
-    SECTION("g can be set") {
-        color.set_g(128);
-        REQUIRE(color.g() == 128);
-    }
-
-    SECTION("b can be set") {
-        color.set_b(128);
-        REQUIRE(color.b() == 128);
-    }
-
-    SECTION("a can be set") {
-        color.set_a(128);
-        REQUIRE(color.a() == 128);
-    }
-}
-
-TEST_CASE("Color structured bindings", "[Color]") {
-
-    SECTION("Colors support structured bindings") {
-        const auto color = Color(1, 2, 3, 4);
-        const auto &[r, g, b, a] = color;
+    SECTION("get()") {
+        const auto [r, g, b, a] = Color(1, 2, 3, 4);
         REQUIRE(r == 1);
         REQUIRE(g == 2);
         REQUIRE(b == 3);
         REQUIRE(a == 4);
     }
-}
     
+    SECTION("format(color, context)") {
+        const auto color = Color(1, 2, 3, 4);
+        const auto expected = "Color(r: 1, g: 2, b: 3, a: 4)";
+        REQUIRE(std::format("{}", color) == expected);
+    }
+}
--- tests/core/PointTests.cpp
+++ tests/core/PointTests.cpp
@@ -2,82 +2,74 @@
 
 #include <seraph/core/Point.hpp>
 
-TEST_CASE("Point construction", "[Point]") {
-    
-    SECTION("default Points are constructed with coordinates (0, 0)") {
+TEST_CASE("Point", "[Point]") {
+
+    SECTION("Point()") {
         const auto point = Point();
-        REQUIRE(point.x() == 0.0f);
-        REQUIRE(point.y() == 0.0f);
+        REQUIRE(point == Point(0.0f, 0.0f));
     }
 
-    SECTION("Points can be constructed memberwise") {
+    SECTION("Point(x, y)") {
         const auto point = Point(1.2f, 3.4f);
-        REQUIRE(point.x() == 1.2f);
-        REQUIRE(point.y() == 3.4f);
+        REQUIRE(point == Point(1.2f, 3.4f));
     }
 
-    SECTION("Points can be copied") {
+    SECTION("Point(other)") {
         const auto point = Point(1.2f, 3.4f);
         const auto other = Point(point);
-        REQUIRE(other.x() == point.x());
-        REQUIRE(other.y() == point.y());
-    }
-}
-
-TEST_CASE("static Points", "[Point]") {
-
-    SECTION("Point::zero() returns the default Point") {
-        REQUIRE(Point::zero() == Point());
-    }
-}
-
-TEST_CASE("Point comparison", "[Point]") {
-
-    SECTION("Points can be checked for exact equality") {
-        REQUIRE(Point() == Point(0.0f, 0.0f));
-        REQUIRE(Point(1.2f, 3.4f) == Point(1.2f, 3.4f));
+        REQUIRE(point == other);
     }
 
-    SECTION("Points can be checked for exact non-equality") {
-        REQUIRE(Point() != Point(1.2f, 3.4f));
-        REQUIRE(Point(1.2f, 3.4f) != Point(3.4f, 1.2f));
+    SECTION("operator<=>(other)") {
+        const auto small = Point(1.2f, 3.4f);
+        const auto large = Point(5.6f, 7.8f);
+        REQUIRE(small < large);
+        REQUIRE(large > small);
+        REQUIRE(small <= small);
+        REQUIRE(small <= large);
+        REQUIRE(large >= small);
+        REQUIRE(large >= large);
+        REQUIRE(small == small);
+        REQUIRE(small != large);
+        REQUIRE((small <=> large) == std::strong_ordering::less);
+        REQUIRE((small <=> small) == std::strong_ordering::equal);
+        REQUIRE((large <=> small) == std::strong_ordering::greater);
     }
 
-    SECTION("Points can be compared memberwise in the order x, y") {
-        REQUIRE(Point(1.2f, 3.4f) < Point(3.4f, 5.6f));
-        REQUIRE(Point(3.4f, 1.2f) > Point(1.2f, 3.4f));
-    }
-}
-
-TEST_CASE("setting Point members after construction", "[Point]") {
-
-    auto point = Point();
-
-    SECTION("x can be set") {
-        point.set_x(1.2f);
+    SECTION("getters") {
+        const auto point = Point(1.2f, 3.4f);
         REQUIRE(point.x() == 1.2f);
-    }
-
-    SECTION("y can be set") {
-        point.set_y(3.4f);
         REQUIRE(point.y() == 3.4f);
     }
-}
 
-TEST_CASE("Point structured bindings", "[Point]") {
+    SECTION("setters") {
+        auto point = Point();
+        point.set_x(1.2f);
+        point.set_y(3.4f);
+        REQUIRE(point == Point(1.2f, 3.4f));
+    }
 
-    SECTION("Points support structured bindings") {
-        const auto point = Point(1.2f, 3.4f);
-        const auto &[x, y] = point;
+    SECTION("get()") {
+        const auto [x, y] = Point(1.2f, 3.4f);
         REQUIRE(x == 1.2f);
         REQUIRE(y == 3.4f);
     }
-}
 
-TEST_CASE("Point arithmetic", "[Point]") {
+    SECTION("format(point, context)") {
+        const auto point = Point(1.2f, 3.4f);
+        const auto expected = "Point(x: 1.2, y: 3.4)";
+        REQUIRE(std::format("{}", point) == expected);
+    }
 
-    SECTION("Points can be multiplied by a floating-point value") {
-        REQUIRE((Point(1.2f, 3.4f) * 5.6f).is_approximately(Point(6.72f, 19.04f)));
+    SECTION("operator+(point, vector)") {
+        const auto point = Point(1.2f, 3.4f);
+        const auto vector = Vector(5.6f, 7.8f);
+        REQUIRE(point + vector == Point(1.2f + 5.6f, 3.4f + 7.8f));
+    }
+
+    SECTION("operator+(point, vector)") {
+        const auto point = Point(1.2f, 3.4f);
+        const auto vector = Vector(5.6f, 7.8f);
+        REQUIRE(point - vector == Point(1.2f - 5.6f, 3.4f - 7.8f));
     }
 }
-    
