b7e931c123b18b9126ec687a4b2a7dd223128857
Jared Payne <jaredmpayne@gmail.com>
2026-05-31 05:30:16 -0400

    Remove stray debug messages

--- seraph/core/Game.cpp
+++ seraph/core/Game.cpp
@@ -11,19 +11,15 @@ int Game::run() {
     auto clock = Clock();
     auto time_since_last_fixed_update = 0.0f;
     while (window().is_open()) {
-        std::cout << "asdf\n";
         EventSystem(m_input, m_window).update();
         update(scene(), clock.time_elapsed());
         time_since_last_fixed_update += clock.restart();
-        std::cout << "updated\n";
         while (time_since_last_fixed_update > time_per_fixed_update()) {
             time_since_last_fixed_update -= time_per_fixed_update();
             scene()->physics_world().update(scene(), time_per_fixed_update());
             fixed_update(scene());
         }
-        std::cout << "physicsdone\n";
         render();
-        std::cout << "renderdone\n";
     }
     return EXIT_SUCCESS;
 }
@@ -48,20 +44,15 @@ void Game::fixed_update(const std::shared_ptr<Node> &node) {
 
 void Game::render() {
     m_window.clear(m_scene->background_color());
-    std::cout << "cleared\n";
     draw(m_scene.get());
     m_window.display();
 }
 
 void Game::draw(Node *node) {
     if (!node->is_hidden()) {
-        std::cout << "drawing node...\n";
         node->draw(m_window);
-        std::cout << "node drawn. drawing children...\n";
         for (const auto &child : node->children()) {
-            std::cout << "drawing child...\n";
             draw(child.get());
         }
-        std::cout << "children drawn\n";
     }
 }
